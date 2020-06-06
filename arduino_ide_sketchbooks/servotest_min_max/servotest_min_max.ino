7c85db32dc6a55035e2ccd9637207192e5d64a2d
Cakemix <mholoch@gmail.com>
2020-06-06 14:41:35 +0200

    added calibration values and using writeMicroseconds

--- arduino_ide_sketchbooks/servotest_min_max/servotest_min_max.ino
+++ arduino_ide_sketchbooks/servotest_min_max/servotest_min_max.ino
@@ -1,18 +1,29 @@
 #include <Servo.h>
 
+int mapRatioOnIntRange(float ratio, int min, int max)
+{
+  const int value_range = max - min;
+  return min + static_cast<int>(static_cast<float>(value_range) * ratio + 0.5f);
+}
+
 // serial output
 const int baud_rate = 9600;
 
-// steering
-const int servo_pin= 7;          // should be MEGA's pin "PWM7"
+// should be MEGA's pin "PWM7"
+const int servo_pin= 7;
 Servo steering_servo;
+
 // use calibration sketch to get these
-const int min_pulse_width = 1000;
-const int max_pulse_width = 2000;
+const int min_pulse_width = 1123; // left
+const int max_pulse_width = 1734; // right
+
+// steering speed
+const float servo_pose_percentage_increment = 0.01;
+const int delay_duration = 15;
 
 // program state
-bool increase_angle = true;
-int servo_angle = 0;
+bool increase_servo_pos = true;
+float servo_pos_percentage = 0.5;
 
 void setup()
 {
@@ -21,40 +32,43 @@ void setup()
 
   // steering
   Serial.println("Setting up servo");
-  steering_servo.attach(servo_pin, min_pulse_width, max_pulse_width);
+  steering_servo.attach(servo_pin);
+  steering_servo.writeMicroseconds(mapRatioOnIntRange(0.5, min_pulse_width, max_pulse_width));
   Serial.print("Servo attached to PIN ");
   Serial.println(servo_pin);
 }
 
 void loop()
 {
-  // steering servo
-  const int angle_increment = 1;
-  const int delay_duration = 15;
-
-  if (increase_angle)
+  if (increase_servo_pos)
   {
-    servo_angle += angle_increment;
-    if (servo_angle >= 180)
+    servo_pos_percentage += servo_pose_percentage_increment;
+    if (servo_pos_percentage >= 1.0f)
     {
-      servo_angle = 180;
-      increase_angle = false;
+      servo_pos_percentage = 1.0f;
+      increase_servo_pos = false;
     }
   }
   else
   {
-    servo_angle -= angle_increment;
-    if (servo_angle <= 0)
+    servo_pos_percentage -= servo_pose_percentage_increment;
+    if (servo_pos_percentage <= 0.0f)
     {
-      servo_angle = 0;
-      increase_angle = true;
+      servo_pos_percentage = 0.0f;
+      increase_servo_pos = true;
     }
   }
 
-  Serial.print("Setting servo to angle ");
-  Serial.print(servo_angle);
-  Serial.println(" degree");
+  const int servo_pulse_width =
+    mapRatioOnIntRange(servo_pos_percentage, min_pulse_width, max_pulse_width);
+  Serial.print("Target: ");
+  Serial.print(servo_pos_percentage);
+  Serial.print(" --> ");
+  Serial.print(servo_pulse_width);
+  Serial.print("ms");
+  Serial.println();
+  steering_servo.writeMicroseconds(servo_pulse_width);
 
-  // delay
+  // setup next loop
   delay(delay_duration);
 }
